db9214bc8c5205462de3321dd4b4de2846d94975
tmmmtmao <2011234h@stu.kobe-u.ac.jp>
2022-10-26 12:26:58 +0900

    change section3-q1

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -1,16 +1,30 @@
 // 練習問題「心臓の鼓動のように大きくなったり小さくなったりする円」
 let count;
 let cycle;
+let size;
 
 function setup(){
-  createCanvas(200, 200);
+  createCanvas(windowWidth, windowHeight);
   count = 0;
   cycle = 100;
 }
 
+function windowResized(){
+  resizeCanvas(windowWidth, windowHeight);
+}
+
 function draw(){
   background(160, 192, 255);
+  size = count 
+  fill(225,0,0)
+  ellipse(width / 2, height / 2, size + 50);
   count = (count + 1) % cycle;
+  if(keyIsDown(" ".charCodeAt(0)))
+  {count = (count + 1.5) % cycle }                      
   // BLANK[1]
-  ellipse(width / 2, height / 2, size);
-}
+  stroke(0)
+  text("my heart", width / 2 - 30, height / 2 - 50 )
+  
+
+}                       
+         
