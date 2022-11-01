d1413170427f0b3042317164f145686a8404b481
tmmmtmao <2011234h@stu.kobe-u.ac.jp>
2022-11-01 12:16:23 +0900

    change section5-q1

--- section5-q1/sketch.js
+++ section5-q1/sketch.js
@@ -4,15 +4,21 @@
 function setup(){
   createCanvas(400, 400);
   background(255);
-  balloon("I love keyakizaka46");
+  balloon("I love keyakizaka45",100,50);
+  
+
 }
 
-function balloon(t){
+function balloon(t,x,y){
   let w = textWidth(t);
   let h = textAscent() + textDescent();
   let p = 2;
+  
+
   fill(0);
-  rect(0, 0, w + p * 2, h + p * 2);
+  rect(x, y, w + p * 2, h + p * 2);
   fill(255);
-  text(t, p, h + p);
+  text(t, p + x, h + p + y);
+  fill(0);
+  triangle(x -30, y + h - 7, x, y + h - 10, x, y + h - 3)
 }
